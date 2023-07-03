90bc34d876c90867345929384683db943fbb2cdd
Efendi <asrulastaga@gmail.com>
2023-07-03 11:17:58 +0700

    -

---
+++ app/src/main/java/com/mnurulefendi202102256/login/ForexRootModel.java
@@ -0,0 +1,20 @@
+package com.mnurulefendi202102256.login;
+
+
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class ForexRootModel
+{
+    @SerializedName("rates")
+    private ForexRatesModel ratesModel;
+
+    public ForexRootModel() {}
+
+    public ForexRatesModel getRatesModel() {
+        return ratesModel; }
+
+    public void setRatesModel(ForexRatesModel ratesModel) {this.ratesModel = ratesModel;}
+}
+
---
+++ app/src/main/java/com/mnurulefendi202102256/login/MenuActivity.java
@@ -0,0 +1,56 @@
+package com.mnurulefendi202102256.login;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private Button _btnMahasiswa, _btnForex;
+    private Intent _tampilMahasiswaIntent, get_tampilForexIntent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        _btnMahasiswa = findViewById(R.id.btnMahasiswa);
+        _btnForex = findViewById(R.id.btnForex);
+
+        initTampilForexButton();
+
+        _btnMahasiswa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AsyncHttpClient asyncHttpClient;
+                String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
+                asyncHttpClient = new AsyncHttpClient();
+
+                asyncHttpClient.get(url, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        Toast.makeText(getApplicationContext(),"Selamat Datang, di Menu Mahasiswa", Toast.LENGTH_SHORT).show();
+                        _tampilMahasiswaIntent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
+                        startActivity(_tampilMahasiswaIntent);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        private void initTampilForexButton
+    }
+}
